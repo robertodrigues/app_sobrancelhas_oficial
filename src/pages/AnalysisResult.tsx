620a3d426ad3335ae121843643e12a382a29de90
[dyad] <wreroberto@gmail.com>
2026-05-22 11:14:09 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/AnalysisResult.tsx
+++ src/pages/AnalysisResult.tsx
@@ -1,1 +1,19 @@
-<h1 className="text-xl font-bold text-center text-slate-900"> {analysis.isComparativo ? 'Relatório de Evolução' : 'Relatório Técnico'} </h1>
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { showSuccess, showError } from '@/utils/toast';
+
+const AnalysisResult = () => {
+const location = useLocation();
+  const { analysis, image, allImages } = location.state || {};
+
+  return (
+    <div className="min-h-screen bg-slate-50 p-6">
+      <h1 className="text-xl font-bold text-center text-slate-900">
+        {analysis.isComparativo ? 'Relatório de Evolução' : 'Relatório Técnico'}
+      </h1>
+      {/* Additional content can be added here based on the analysis data */}
+    </div>
+  );
+};
+
+export default AnalysisResult;
