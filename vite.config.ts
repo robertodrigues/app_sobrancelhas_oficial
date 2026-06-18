8cf9882f4c73ae578c7c6d5a79305e2715331858
[dyad] <wreroberto@gmail.com>
2026-06-18 10:10:47 -0300

    [dyad] Fixing Nitro Vite import - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import nitro from "nitro/vite";
+import { nitro } from "nitro/vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
